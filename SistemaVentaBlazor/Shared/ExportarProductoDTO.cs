27751f303a933c8920e6a158fcb38058f18048b4
Jorge Eduardo Tamayo Ramos <jtamayoramos@gmail.com>
2025-04-23 23:20:50 -0500

    Exportar productos

---
+++ SistemaVentaBlazor/Shared/ExportarProductoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaVentaBlazor.Shared
+{
+    public class ExportarProductoDTO
+    {
+        public string? Nombre { get; set; }
+        public string? DescripcionCategoria { get; set; }
+        public int? Stock { get; set; }
+        public DateTime? FechaVencimiento { get; set; }
+    }
+}
